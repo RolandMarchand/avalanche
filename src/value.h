60ea5470902cff8f4bd01fb638d29c7404296e87
Roland Marchand <roland.marchand@protonmail.com>
2022-08-02 11:40:00 -0400

    Changed value

--- src/value.h
+++ src/value.h
@@ -25,10 +25,15 @@
 
 #include <stdint.h>
 
-#define GET_VALUE_NUMBER(num)					\
+#define GET_VALUE_INT(num)					\
 	((struct value){					\
-		.as.number = (num),				\
-		.type = VALUE_NUMBER				\
+		.as.integer = (int)(num),			\
+		.type = VALUE_INT				\
+	})
+#define GET_VALUE_FLOAT(num)					\
+	((struct value){					\
+		.as.float_p = (double)(num),			\
+		.type = VALUE_FLOAT				\
 	})
 #define GET_VALUE_BOOL(boolean)					\
 	((struct value) {					\
@@ -37,30 +42,17 @@
 	})
 
 enum value_type {
-	VALUE_NUMBER = 0,
+	VALUE_INT = 0,
+	VALUE_FLOAT,
 	VALUE_BOOL,
-	VALUE_NIL
 };
 
 struct value {
 	union {
-		double number;
+		int integer;
+		double float_p;
 		uint8_t bool;
-		void *structure;
+		void *structure; /* used for later */
 	} as;
 	enum value_type type;
 };
-
-struct value value_negate(struct value val);
-struct value value_logical_not(struct value val);
-struct value value_add(struct value val1, struct value val2);
-struct value value_substract(struct value val1, struct value val2);
-struct value value_multiply(struct value val1, struct value val2);
-struct value value_divide(struct value val1, struct value val2);
-struct value value_modulo(struct value val1, struct value val2);
-struct value value_greater(struct value val1, struct value val2);
-struct value value_greater_or_equal(struct value val1, struct value val2);
-struct value value_less(struct value val1, struct value val2);
-struct value value_less_or_equal(struct value val1, struct value val2);
-struct value value_equal(struct value val1, struct value val2);
-struct value value_not_equal(struct value val1, struct value val2);
